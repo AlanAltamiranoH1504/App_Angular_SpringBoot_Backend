48de738374cab8cb575bbaea5e4f7285ae8f718c
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-16 11:22:12 -0600

    commit initial

---
+++ src/main/java/altamirano/hernandez/app_springboot_react/AppSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.app_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringBootReactApplication.class, args);
+    }
+
+}
