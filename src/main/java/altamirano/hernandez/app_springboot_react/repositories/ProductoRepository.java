87a4e2abdd6c7165c4febafb1631e79b8fed32b6
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-16 12:12:41 -0600

    feat: add configuration for RestResource repository
    - add configuration for repository of type RestResource
    
    - expose ids in repository responses through configuration class

--- src/main/java/altamirano/hernandez/app_springboot_react/repositories/ProductoRepository.java
+++ src/main/java/altamirano/hernandez/app_springboot_react/repositories/ProductoRepository.java
@@ -2,6 +2,9 @@ package altamirano.hernandez.app_springboot_react.repositories;
 
 import altamirano.hernandez.app_springboot_react.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//Configuramos para uso rapido con cliente
+@RepositoryRestResource(path = "productos")
 public interface ProductoRepository extends CrudRepository<Producto, Integer> {
 }
