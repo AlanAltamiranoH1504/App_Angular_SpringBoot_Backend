b26b34e6969ac265e8115e5f8aad731ebe138f2d
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-16 13:43:41 -0600

    feat: add CORS enablement
    - add CORS enablement for React client

--- src/main/java/altamirano/hernandez/app_springboot_react/repositories/ProductoRepository.java
+++ src/main/java/altamirano/hernandez/app_springboot_react/repositories/ProductoRepository.java
@@ -3,8 +3,10 @@ package altamirano.hernandez.app_springboot_react.repositories;
 import altamirano.hernandez.app_springboot_react.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //Configuramos para uso rapido con cliente
+@CrossOrigin(origins = "http://localhost:5173/")
 @RepositoryRestResource(path = "productos")
 public interface ProductoRepository extends CrudRepository<Producto, Integer> {
 }
